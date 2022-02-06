0aaa85a0e880cc336b457414fc5421a8d7857ad3
wangran <wangran@gmail.com>
2022-02-06 12:16:16 +0800

    Add pandas dataframe conversion in the sample code.

--- examples/ccxt-ohlcv.py
+++ examples/ccxt-ohlcv.py
@@ -1,5 +1,6 @@
 import ccxt
 import os
+import pandas as pd
 from datetime import datetime
 from typing import Any, List
 
@@ -18,8 +19,13 @@ def fetch_ohlcv(symbol: str, granularity: str, start: datetime, limit: int) -> L
     since = int((start - datetime(1970, 1, 1)).total_seconds() * 1000)
     return exchange.fetch_ohlcv(symbol, timeframe=granularity, since=since, limit=limit)
 
+def to_ohlcv_data_frame(data):
+    df = pd.DataFrame(data, columns=['time', 'open', 'high', 'low', 'close', 'volume'])
+    df.time = df.time.apply(lambda x: datetime.fromtimestamp(x / 1000))
+    return df
 
 if __name__ == "__main__":
     cache = TimeSeriesCache(os.path.join(os.path.dirname(__file__), "cache"), fetch_ohlcv, 1500)
     result = cache.query("BTC-PERPETUAL", "15m", datetime(2020, 1, 1), datetime.today())
     print("Cache saved to %s (%d points)" % (cache.basedir, len(result)))
+    print(to_ohlcv_data_frame(result))
